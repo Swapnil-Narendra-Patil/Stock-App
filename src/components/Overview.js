65321eb40756ee78e34fe5dfe1b9e0a3a6ed3e80
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Air-2.local>
2024-06-05 16:43:55 -0400

    Fixed UI Bugs

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,20 +7,21 @@ const Overview = ({ symbol, price, change, changePercent, currency }) => {
       <span className="absolute left-2 top-2 text-neutral-400 text-lg xl:text-xl 2xl:text-2xl">
         {symbol}
       </span>
-      <div className="w-full h-full flex items-center justify-around">
-        <span className="pt-2 text-2xl xl:text-4xl 2xl:text-5xl flex items-center">
-          ${price}
+      <div className="w-full h-full flex flex-col md:flex-row items-center justify-around text-center md:text-left">
+        <div className="flex items-center justify-center md:justify-start flex-wrap">
+          <span className="pt-2 text-2xl xl:text-4xl 2xl:text-5xl">
+            ${price}
+          </span>
           <span className="pt-2 text-lg xl:text-xl 2xl:text-2xl text-neutral-400 m-2">
             {currency}
           </span>
-        </span>
-        <span
-          className={`pt-2 text-lg xl:text-xl 2xl:text-2xl ${
-            change > 0 ? "text-lime-500" : "text-red-500"
-          }`}
-        >
-          {change} <span>({changePercent}%)</span>
-        </span>
+        </div>
+        <div className="pt-2 text-lg xl:text-xl 2xl:text-2xl flex items-center">
+          <span className={`${change > 0 ? "text-lime-500" : "text-red-500"}`}>
+            {change}
+          </span>
+          <span className="text-neutral-400 ml-1">({changePercent}%)</span>
+        </div>
       </div>
     </Card>
   );
