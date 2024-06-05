6456b509d6678190fb48a2b26609d94574def7d7
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Air-2.local>
2024-06-04 20:54:02 -0400

    Fixed paddings

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -8,14 +8,14 @@ const Overview = ({ symbol, price, change, changePercent, currency }) => {
         {symbol}
       </span>
       <div className="w-full h-full flex items-center justify-around">
-        <span className="text-2xl xl:text-4xl 2xl:text-5xl flex items-center">
+        <span className="pt-2 text-2xl xl:text-4xl 2xl:text-5xl flex items-center">
           ${price}
-          <span className="text-lg xl:text-xl 2xl:text-2xl text-neutral-400 m-2">
+          <span className="pt-2 text-lg xl:text-xl 2xl:text-2xl text-neutral-400 m-2">
             {currency}
           </span>
         </span>
         <span
-          className={`text-lg xl:text-xl 2xl:text-2xl ${
+          className={`pt-2 text-lg xl:text-xl 2xl:text-2xl ${
             change > 0 ? "text-lime-500" : "text-red-500"
           }`}
         >
