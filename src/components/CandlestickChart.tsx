786959e639e5c35f2b80e6185971bc4a9ca176fd
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Air-2.local>
2024-06-05 15:59:55 -0400

    Fixed Bugs

--- src/components/CandlestickChart.tsx
+++ src/components/CandlestickChart.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import {
   Chart as ChartJS,
   registerables,
@@ -9,19 +9,19 @@ import {
   CategoryScale,
   ChartData,
   ChartOptions,
-} from 'chart.js';
-import annotationPlugin from 'chartjs-plugin-annotation';
-import 'chartjs-adapter-date-fns';
+} from "chart.js";
+import annotationPlugin from "chartjs-plugin-annotation";
+import "chartjs-adapter-date-fns";
 import {
   CandlestickController,
   CandlestickElement,
-} from 'chartjs-chart-financial';
-import { Chart } from 'react-chartjs-2';
-import { StockData } from '../data/dummyData';
-import { IntradayStockData } from '../data/dummyIntradayData';
-import { format } from 'date-fns';
-import ChartFilter from './ChartFilter';
-import ThemeContext from '../context/ThemeContext';
+} from "chartjs-chart-financial";
+import { Chart } from "react-chartjs-2";
+import { StockData } from "../data/dummyData";
+import { IntradayStockData } from "../data/dummyIntradayData";
+import { format } from "date-fns";
+import ChartFilter from "./ChartFilter";
+import ThemeContext from "../context/ThemeContext"; // Adjust the import path as necessary
 
 ChartJS.register(
   ...registerables,
@@ -40,59 +40,89 @@ interface CandlestickChartProps {
   intradayData: IntradayStockData[];
 }
 
-const CandlestickChart: React.FC<CandlestickChartProps> = ({ dailyData, intradayData }) => {
-  const { darkMode } = useContext(ThemeContext);
-  const [filteredData, setFilteredData] = useState<StockData[] | IntradayStockData[]>([]);
-  const [filter, setFilter] = useState('1D'); // Default filter to 1 Day
-
-  useEffect(() => {
-    filterData(filter);
-  }, [filter, dailyData, intradayData]);
+const CandlestickChart: React.FC<CandlestickChartProps> = ({
+  dailyData,
+  intradayData,
+}) => {
+  const [filteredData, setFilteredData] = useState<
+    StockData[] | IntradayStockData[]
+  >(intradayData);
+  const [filter, setFilter] = useState("1D"); // Default filter to 1 Day
+  const { darkMode } = useContext(ThemeContext); // Use the dark mode context
 
   const filterData = (interval: string) => {
     let filtered: StockData[] | IntradayStockData[] = [];
     switch (interval) {
-      case '1D':
+      case "1D":
         filtered = intradayData;
         break;
-      case '1W':
+      case "1W":
         filtered = dailyData.slice(-7); // Filter last 7 days
         break;
-      case '1M':
+      case "1M":
         filtered = dailyData.slice(-30); // Filter last 30 days
         break;
-      case '3M':
+      case "3M":
         filtered = dailyData.slice(-90); // Filter last 90 days
         break;
-      case '5M':
+      case "5M":
         filtered = dailyData.slice(-150); // Filter last 150 days
         break;
-      case 'YTD':
-        filtered = dailyData.filter(item => new Date(item.date).getFullYear() === new Date().getFullYear());
+      case "YTD":
+        filtered = dailyData.filter(
+          (item) =>
+            new Date(item.date).getFullYear() === new Date().getFullYear()
+        );
         break;
-      case '1Y':
+
+      case "1Y":
         filtered = dailyData.slice(-365); // Filter last 365 days
         break;
       default:
         filtered = dailyData;
     }
-    console.log(`Filtered Data for ${interval}:`, filtered);
     setFilteredData(filtered);
   };
 
   const getTimeUnit = (filter: string) => {
-    return filter === '1D' ? 'minute' : 'day';
+    return filter === "1D" ? "minute" : "day";
   };
 
-  const latestDataPoint = filteredData.length > 0 ? filteredData[filteredData.length - 1] : null;
-  const isPriceIncreasing = latestDataPoint !== null && latestDataPoint.close > latestDataPoint.open;
+  const getStepSize = (filter: string) => {
+    switch (filter) {
+      case "1D":
+        return 30; // 30 minutes
+      case "1W":
+        return 1; // 1 day
+      case "1M":
+        return 2; // 2 days
+      case "3M":
+        return 7; // 1 week
+      case "5M":
+        return 14; // 2 weeks
+      case "YTD":
+        return 30; // 1 month
+      case "1Y":
+        return 30; // 1 month
+      default:
+        return 1;
+    }
+  };
 
-  const chartData: ChartData<'candlestick'> = {
+  const latestDataPoint =
+    filteredData.length > 0 ? filteredData[filteredData.length - 1] : null;
+  const isPriceIncreasing =
+    latestDataPoint !== null && latestDataPoint.close > latestDataPoint.open;
+
+  const chartData: ChartData<"candlestick"> = {
     datasets: [
       {
-        label: 'Stock Price',
-        data: filteredData.map(item => ({
-          x: 'time' in item ? new Date(`${item.date}T${item.time}`).getTime() : new Date(item.date).getTime(),
+        label: "Stock Price",
+        data: filteredData.map((item) => ({
+          x:
+            "time" in item
+              ? new Date(`${item.date}T${item.time}`).getTime()
+              : new Date(item.date).getTime(),
           o: item.open,
           h: item.high,
           l: item.low,
@@ -101,53 +131,54 @@ const CandlestickChart: React.FC<CandlestickChartProps> = ({ dailyData, intraday
         borderColor: (context) => {
           const { dataset, dataIndex } = context;
           const value = dataset.data[dataIndex] as any;
-          return value?.c > value?.o ? 'rgb(0, 255, 0)' : 'rgb(255, 0, 0)'; // Fully opaque green and red colors
+          return value?.c > value?.o ? "rgb(0, 255, 0)" : "rgb(255, 0, 0)"; // Fully opaque green and red colors
         },
         backgroundColor: (context) => {
           const { dataset, dataIndex } = context;
           const value = dataset.data[dataIndex] as any;
-          return value?.c > value?.o ? 'rgb(0, 255, 0)' : 'rgb(255, 0, 0)'; // Fully opaque green and red colors
+          return value?.c > value?.o ? "rgb(0, 255, 0)" : "rgb(255, 0, 0)"; // Fully opaque green and red colors
         },
       },
     ],
   };
+  const timeUnit = getTimeUnit(filter);
 
-  const chartOptions: ChartOptions<'candlestick'> = {
+  const chartOptions: ChartOptions<"candlestick"> = {
     responsive: true,
     maintainAspectRatio: false,
     scales: {
       x: {
-        type: 'time',
+        type: "time",
         time: {
-          unit: getTimeUnit(filter),
-          tooltipFormat: filter === '1D' ? 'MMM d, yyyy HH:mm' : 'MMM d, yyyy',
+          unit: timeUnit,
+          tooltipFormat: filter === "1D" ? "MMM d, yyyy HH:mm" : "MMM d, yyyy",
           displayFormats: {
-            minute: 'HH:mm',
-            hour: 'HH:mm',
-            day: 'MMM d',
-            week: 'MMM d',
-            month: 'MMM yyyy',
-            quarter: 'MMM yyyy',
-            year: 'yyyy',
+            minute: "HH:mm",
+            hour: "HH:mm",
+            day: "MMM d",
+            week: "MMM d",
+            month: "MMM yyyy",
+            quarter: "MMM yyyy",
+            year: "yyyy",
           },
-          minUnit: 'minute',
+          minUnit: "minute",
         },
         grid: {
-          color: darkMode ? '#555555' : '#EDEDED', // Change grid color based on dark mode
+          color: darkMode ? "#444" : "#EDEDED", // Dark mode grid line color
         },
         ticks: {
-          color: darkMode ? '	#C0C0C0' : '#000000', // Change tick color based on dark mode
           autoSkip: true,
-          maxTicksLimit: filter === '1D' ? 12 : 10, // Adjusted limit for intraday
+          color: darkMode ? "#fff" : "#000", // Change tick color based on dark mode
+          maxTicksLimit: filter === "1D" ? 12 : 10, // Adjusted limit for intraday
         },
       },
       y: {
         beginAtZero: false,
         grid: {
-          color: darkMode ? '#555555' : '#EDEDED', // Change grid color based on dark mode
+          color: darkMode ? "#444" : "#EDEDED", // Dark mode grid line color
         },
         ticks: {
-          color: darkMode ? '	#C0C0C0' : '#000000', // Change tick color based on dark mode
+          color: darkMode ? "#fff" : "#000", // Change tick color based on dark mode
         },
       },
     },
@@ -159,7 +190,10 @@ const CandlestickChart: React.FC<CandlestickChartProps> = ({ dailyData, intraday
         callbacks: {
           label: (context) => {
             const dataPoint = context.raw as any;
-            const date = format(new Date(dataPoint.x), filter === '1D' ? 'MMM d, yyyy HH:mm' : 'MMM d, yyyy');
+            const date = format(
+              new Date(dataPoint.x),
+              filter === "1D" ? "MMM d, yyyy HH:mm" : "MMM d, yyyy"
+            );
             return `Date: ${date}\nOpen: ${dataPoint.o}\nHigh: ${dataPoint.h}\nLow: ${dataPoint.l}\nClose: ${dataPoint.c}`;
           },
         },
@@ -168,18 +202,14 @@ const CandlestickChart: React.FC<CandlestickChartProps> = ({ dailyData, intraday
         annotations: {
           latestPriceLine: latestDataPoint
             ? {
-                type: 'line',
+                type: "line",
                 yMin: latestDataPoint.close,
                 yMax: latestDataPoint.close,
-                borderColor: isPriceIncreasing ? 'rgb(0, 255, 0)' : 'rgb(255, 0, 0)',
-                borderWidth: 1,
+                borderColor: isPriceIncreasing
+                  ? "rgb(0, 255, 0)"
+                  : "rgb(255, 0, 0)",
+                borderWidth: 2,
                 borderDash: [5, 5], // Make the line dotted
-                label: {
-                  content: latestDataPoint.close.toString(),
-                  position: 'end',
-                  backgroundColor: isPriceIncreasing ? 'rgb(0, 255, 0)' : 'rgb(255, 0, 0)',
-                  color: '#ffffff',
-                },
               }
             : undefined,
         },
@@ -190,17 +220,20 @@ const CandlestickChart: React.FC<CandlestickChartProps> = ({ dailyData, intraday
   return (
     <div>
       <div className="mb-4 flex space-x-2">
-        {['1D', '1W', '1M', '3M', '5M', 'YTD', '1Y'].map((interval) => (
+        {["1D", "1W", "1M", "3M", "5M", "YTD", "1Y"].map((interval) => (
           <ChartFilter
             key={interval}
             text={interval}
             active={filter === interval}
-            onClick={() => setFilter(interval)}
+            onClick={() => {
+              filterData(interval);
+              return setFilter(interval);
+            }}
           />
         ))}
       </div>
 
-      <div style={{ height: '40rem', width: '100%' }}>
+      <div style={{ height: "40rem", width: "100%" }}>
         {filteredData.length > 0 ? (
           <Chart type="candlestick" data={chartData} options={chartOptions} />
         ) : (
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import Card from "./Card";
-import FinancialTable from './FinancialTable';
-import { fetchFinancialData } from '../services/api.tsx';
-import CandlestickChart from './CandlestickChart.tsx';
-import Header from './Header.js'
-import Overview from './Overview.js'
-import ErrorBoundary from './ErrorBoundry.tsx';
+import FinancialTable from "./FinancialTable";
+import { fetchFinancialData } from "../services/api.tsx";
+import CandlestickChart from "./CandlestickChart.tsx";
+import Header from "./Header.js";
+import Overview from "./Overview.js";
+import ErrorBoundary from "./ErrorBoundry.tsx";
 import ThemeContext from "../context/ThemeContext";
-import { mockCompanyDetails } from '../constants/mock.js';
-import { dummyData } from '../data/dummyData.tsx';
-import { dummyIntradayData } from '../data/dummyIntradayData.tsx';
-
-
-
-
+import { mockCompanyDetails } from "../constants/mock.js";
+import { dummyData } from "../data/dummyData.tsx";
+import { dummyIntradayData } from "../data/dummyIntradayData.tsx";
 
 const Dashboard = () => {
   const [data, setData] = useState(null);
-const { darkMode } = useContext(ThemeContext);
+  const { darkMode } = useContext(ThemeContext);
 
   useEffect(() => {
     const getData = async () => {
@@ -28,36 +24,48 @@ const { darkMode } = useContext(ThemeContext);
     getData();
   }, []);
 
+  // Calculate the latest stock price, change, and change percent
+  const latestPrice = dummyData[dummyData.length - 1]?.close || 0;
+  const previousPrice = dummyData[dummyData.length - 2]?.close || 0;
+  const change = latestPrice - previousPrice;
+  const changePercent = previousPrice ? (change / previousPrice) * 100 : 0;
+
   if (!data) return <div>Loading...</div>;
 
   return (
-    <div className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-5 px-10 font-quicksand ${darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100"}`}>
-
+    <div
+      className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-5 px-10 font-quicksand ${
+        darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100"
+      }`}
+    >
       <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
         <Header name={mockCompanyDetails.name} />
       </div>
       <div className="md:col-span-2 xl:col-span-2 row-span-5 overflow-hidden">
         <Card>
           <ErrorBoundary>
-          <CandlestickChart dailyData={dummyData} intradayData={dummyIntradayData} />
+            <CandlestickChart
+              dailyData={dummyData}
+              intradayData={dummyIntradayData}
+            />
           </ErrorBoundary>
         </Card>
       </div>
       <div>
         <ErrorBoundary>
-        <Overview
-          symbol={mockCompanyDetails.ticker}
-          price={300}
-          change={30}
-          changePercent={10.0}
-          currency={"USD"}
-        />
+          <Overview
+            symbol={mockCompanyDetails.ticker}
+            price={latestPrice.toFixed(2)}
+            change={change.toFixed(2)}
+            changePercent={changePercent.toFixed(2)}
+            currency={"USD"}
+          />
         </ErrorBoundary>
       </div>
       <div className="md:col-span-1 xl:col-span-1 row-span-4 overflow-auto">
         <Card>
-        <ErrorBoundary>
-          <FinancialTable data={data} />
+          <ErrorBoundary>
+            <FinancialTable data={data} />
           </ErrorBoundary>
         </Card>
       </div>
