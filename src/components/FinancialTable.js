2a525336423b461fcd0a4b53085f0bc9c84e1919
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Air-2.local>
2024-06-06 05:39:11 -0400

    New Table

--- src/components/FinancialTable.js
+++ src/components/FinancialTable.js
@@ -4,6 +4,39 @@ import ThemeContext from "../context/ThemeContext";
 const FinancialTable = ({ data }) => {
   const { darkMode } = useContext(ThemeContext);
 
+  // Default data in case actual data is not fetched
+  const defaultData = {
+    analyst_estimates: {
+      Citibank: 6.5,
+      "Goldman Sachs": 7.9,
+      "Morgan Stanley": 9.87,
+    },
+    current_ratio: 7.1,
+    debt_to_equity_ratio: 2.1,
+    eps: 1.7,
+    market_ap: 2.5,
+    news: {
+      article1: {
+        sentiment: { score: 0.9, value: "positive" },
+        summary: "This is Article1",
+      },
+      article2: {
+        sentiment: { score: 0.67, value: "negative" },
+        summary: "This is Article2",
+      },
+      article3: {
+        sentiment: { score: 0.559, value: "positive" },
+        summary: "This is Article3",
+      },
+    },
+    pb_ratio: 7.9,
+    pe_ratio: 1.2,
+    peg_ratio: 5.5,
+    ps_ratio: 33.5,
+    shares_outstanding: 317,
+  };
+
+  // Use default data if actual data is not available
   const {
     analyst_estimates,
     current_ratio,
@@ -15,8 +48,8 @@ const FinancialTable = ({ data }) => {
     peg_ratio,
     ps_ratio,
     shares_outstanding,
-    news, // Add news data here
-  } = data;
+    news,
+  } = data || defaultData;
 
   // Additional analysts and their estimates
   const additional_analysts = {
