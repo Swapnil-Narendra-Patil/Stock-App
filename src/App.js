023546698088f135a815d35a4a28dd2ddb6b75bc
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Air-2.local>
2024-06-06 04:53:43 -0400

    News 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // src/App.js
 
-import React, { useEffect, useState } from "react";
-import { dummyData } from "./data/dummyData.tsx";
+import React, { useEffect, useState, useContext } from "react";
 import { fetchFinancialData } from "./services/api.tsx";
 import Dashboard from "./components/Dashboard.js";
-import Candlestick from "./components/CandlestickChart.tsx";
+import News from "./components/News.js";
 import ThemeContext from "./context/ThemeContext.js";
 
 const App = () => {
   const [data, setData] = useState(null);
   const [darkMode, setDarkMode] = useState(false);
+
   useEffect(() => {
     const getData = async () => {
       const result = await fetchFinancialData();
@@ -23,9 +23,16 @@ const App = () => {
 
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <>
+      <div
+        className={`h-screen overflow-y-auto ${
+          darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100 text-black"
+        }`}
+      >
         <Dashboard />
-      </>
+        <div className="p-5">
+          <News news={data.news} />
+        </div>
+      </div>
     </ThemeContext.Provider>
   );
 };
